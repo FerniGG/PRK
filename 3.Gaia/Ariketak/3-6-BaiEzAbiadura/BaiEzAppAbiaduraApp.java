76030f699511e52d0ba979a9e058fb651cc30cf3
FerniGG <FerniGG@github.com>
2023-02-06 22:43:47 +0100

    subir .jars de una puta vez

---
+++ 3.Gaia/Ariketak/3-6-BaiEzAbiadura/BaiEzAppAbiaduraApp.java
@@ -0,0 +1,52 @@
+public class BaiEzAppAbiaduraApp{
+	public static void main (String args[]) {
+		Pantaila p = new Pantaila();
+		Idazlea bai = new Idazlea("bai",500,25,p);
+		Idazlea ez = new Idazlea("\tez",500,10,p);
+		bai.start();
+		ez.start();
+		}
+		}
+
+		class Idazlea extends Thread {
+			private String baiEz;
+			private int kopurua;
+			private double denbora;
+			private Pantaila p;
+			public Idazlea(String s, float i, int j, Pantaila pant){
+			baiEz=s; denbora=i; kopurua=j;
+			p=pant;
+			}
+			public void run() {
+				try {
+				 int i;
+				 for (i=0;i<kopurua;i++) {
+				p.margotu(baiEz);
+		
+				sleep((int)denbora);
+				 if(this.baiEz.compareTo("bai")==0) {
+					 this.denbora=this.denbora/1.2;
+				 }
+				 else {
+					 this.denbora=this.denbora*1.2;
+				 }
+				 }
+				 p.margotu(baiEz+" bukatu da");
+				
+				}catch (InterruptedException e) {}
+				}
+		
+		
+			}
+
+class Pantaila {
+			public Pantaila() {
+				
+			}
+		
+			public void margotu(String s){
+				System.out.println(s);
+				}
+			
+				}
+		
